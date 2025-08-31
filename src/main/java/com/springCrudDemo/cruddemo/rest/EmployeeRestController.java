453402fd6e9f6bc8b63fb28ebffaf0ba91565fe5
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-08-31 22:15:57 +0300

    commit 3

--- src/main/java/com/springCrudDemo/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/springCrudDemo/cruddemo/rest/EmployeeRestController.java
@@ -1,11 +1,13 @@
 package com.springCrudDemo.cruddemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springCrudDemo.cruddemo.entity.Employee;
 import com.springCrudDemo.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping
@@ -14,11 +16,15 @@ public class EmployeeRestController {
 
     private EmployeeService employeeService;
 
+    private ObjectMapper objectMapper;
+
+
     // quick and dirty : inject employee dao ( use constructor injection)
 
     @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService){
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper){
         employeeService = theEmployeeService;
+        objectMapper = theObjectMapper;
     }
 
     // expose "/employees" and return a list of employees
@@ -43,7 +49,7 @@ public class EmployeeRestController {
 
     // add mapping for POST / employees - add new employee
 
-    @PostMapping("/employees/{employeeId}")
+    @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee theEmployee){
 
         // also just in case they pass an id in JSON ... set id to 0
@@ -56,4 +62,38 @@ public class EmployeeRestController {
         return dbEmployee;
 
     }
+
+    // add mapping for PUT /employees - update existing employee
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PATCH /employees/{employeeId} - patch employee ... partial update
+
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId,
+                                  @RequestBody Map<String, Object> patchPayload){
+        Employee tempEmploee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if(tempEmploee == null) {
+            throw  new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        if(patchPayload.containsKey("id")){
+            throw  new RuntimeException("Employee id nit allowed in request body -" + employeeId);
+        }
+
+        Employee patchedEmployee = apply(patchPayload , tempEmploee);
+    }
+    private Employee apply(Map<String, Object>patchPayload,Employee tempEmployee){
+
+    }
+
+
+
 }
